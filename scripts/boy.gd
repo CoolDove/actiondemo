9326ff2e17a8de2000d6f0e1e973d8717a138642
Dove <1515098414@qq.com>
2026-08-09 19:11:50 +0800

    polish character movement

--- scripts/boy.gd
+++ scripts/boy.gd
@@ -8,8 +8,12 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_anchor = %CameraAnchor
 
-@onready var camera_pivot : Node3D
-@onready var camera : Camera3D
+var camera_input : Vector2
+
+#< Should be initialized outside
+var camera_pivot : Node3D
+var camera : Camera3D
+#>
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -20,19 +24,25 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		var horizontal :float= -event.relative.x * mouse_sensitivity
 		var vertical   :float= -event.relative.y * mouse_sensitivity
-		camera_pivot.global_rotate(Vector3.UP, horizontal)
-		camera_pivot.global_rotate(camera_pivot.basis.x, vertical)
-		var euler := camera_pivot.global_basis.get_euler(EULER_ORDER_YXZ)
-		euler.x = clamp(euler.x, deg_to_rad(-30.0), deg_to_rad(30.0))
-		camera_pivot.global_rotation = euler
+		camera_input = Vector2(horizontal, vertical)
 	if event is InputEventKey and event.keycode == KEY_ESCAPE and event.pressed:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func _process(delta):
+	camera_input = Vector2.ZERO
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	if camera_pivot != null:
+		var mouse_sens := 100.0
+		camera_pivot.global_rotate(Vector3.UP, camera_input.x * mouse_sens * delta)
+		camera_pivot.global_rotate(camera_pivot.basis.x, camera_input.y * mouse_sens * delta)
+		var euler := camera_pivot.global_basis.get_euler(EULER_ORDER_YXZ)
+		euler.x = clamp(euler.x, deg_to_rad(-30.0), deg_to_rad(30.0))
+		camera_pivot.global_rotation = euler
+		
 		var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 		var cam_forward = camera_pivot.global_transform.basis.z
 		cam_forward.y = 0
@@ -40,12 +50,13 @@ func _physics_process(delta):
 		var cam_right = camera_pivot.global_transform.basis.x
 		cam_right.y = 0
 		cam_right = cam_right.normalized()
+		var chr_forward = global_transform.basis.z
 
-		var direction = (cam_right * input_dir.x + cam_forward * -input_dir.y).normalized()
-		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
-			rotation.y = lerp_angle(rotation.y, atan2(direction.x, direction.z), rotation_speed * delta)
+		var target_direction = (cam_right * input_dir.x + cam_forward * -input_dir.y).normalized()
+		if input_dir:
+			velocity.x = chr_forward.x * speed
+			velocity.z = chr_forward.z * speed
+			rotation.y = lerp_angle(rotation.y, atan2(target_direction.x, target_direction.z), rotation_speed * delta)
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
 			velocity.z = move_toward(velocity.z, 0, speed)
