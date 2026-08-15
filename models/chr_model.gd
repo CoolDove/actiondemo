9d7a38c0794592faf4703c18677c6940a9de97fa
Dove <1515098414@qq.com>
2026-08-15 23:15:21 +0800

    move driven by root motion

--- models/chr_model.gd
+++ models/chr_model.gd
@@ -1,4 +1,17 @@
 extends Node3D
 class_name ChrModel
 
-@onready var anim_tree = %AnimationTree
+@onready var armature :Node3D= %Armature
+@onready var anim_player :AnimationPlayer= %AnimationPlayer
+@onready var anim_tree :AnimationTree= %AnimationTree
+@onready var skeleton_3d :Skeleton3D= %Skeleton3D
+var boneid_root :int
+
+func _ready():
+	boneid_root = skeleton_3d.find_bone("root")
+
+func get_root_motion_position() -> Vector3:
+	return armature.global_transform.basis * anim_tree.get_root_motion_position()
+
+func get_root_motion_rotation() -> Quaternion:
+	return armature.quaternion * anim_tree.get_root_motion_rotation()
--- scripts/boy.gd
+++ scripts/boy.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-@export var speed = 5.0
 @export var mouse_sensitivity = 0.002
 @export var rotation_speed = 10.0
 
@@ -8,16 +7,13 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_anchor = %CameraAnchor
 
-@onready var model :ChrModel= $Model
-@onready var anim_tree :AnimationPlayer:
-	get: return model.anim_tree
+@onready var model: ChrModel = $Model
+@onready var anim_tree :AnimationTree= model.anim_tree
 
-var camera_input : Vector2
+var camera_input: Vector2
 
-#< Should be initialized outside
-var camera_pivot : Node3D
-var camera : Camera3D
-#>
+var camera_pivot: Node3D
+var camera: Camera3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -32,7 +28,7 @@ func _unhandled_input(event):
 	if event is InputEventKey and event.keycode == KEY_ESCAPE and event.pressed:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-func _process(delta):
+func _process(_delta):
 	camera_input = Vector2.ZERO
 
 func _physics_process(delta):
@@ -46,7 +42,7 @@ func _physics_process(delta):
 		var euler := camera_pivot.global_basis.get_euler(EULER_ORDER_YXZ)
 		euler.x = clamp(euler.x, deg_to_rad(-30.0), deg_to_rad(30.0))
 		camera_pivot.global_rotation = euler
-		
+
 		var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 		var cam_forward = camera_pivot.global_transform.basis.z
 		cam_forward.y = 0
@@ -54,14 +50,21 @@ func _physics_process(delta):
 		var cam_right = camera_pivot.global_transform.basis.x
 		cam_right.y = 0
 		cam_right = cam_right.normalized()
-		var chr_forward = global_transform.basis.z
 
 		var target_direction = (cam_right * input_dir.x + cam_forward * -input_dir.y).normalized()
+
 		if input_dir:
-			velocity.x = chr_forward.x * speed
-			velocity.z = chr_forward.z * speed
+			anim_tree.set("parameters/IdleWalk/blend_position", 1.0)
 			rotation.y = lerp_angle(rotation.y, atan2(target_direction.x, target_direction.z), rotation_speed * delta)
 		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			velocity.z = move_toward(velocity.z, 0, speed)
+			anim_tree.set("parameters/IdleWalk/blend_position", 0.0)
+
+		var velocity_y := velocity.y
+		# if input_dir:
+		var root_motion_position :Vector3= model.get_root_motion_position()
+		velocity = root_motion_position / delta
+		# else:
+		# 	velocity = Vector3.ZERO
+		velocity.y = velocity_y
+		# print("velocity: %s" % [ velocity ])
 	move_and_slide()
