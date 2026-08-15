f872e8eddbe4fe145ba184e070a9887ead17afc2
Dove <1515098414@qq.com>
2026-08-15 23:29:04 +0800

    update

--- scripts/boy.gd
+++ scripts/boy.gd
@@ -1,16 +1,21 @@
 extends CharacterBody3D
 
-@export var mouse_sensitivity = 0.002
-@export var rotation_speed = 10.0
+@export_category("Basic")
+@export var mouse_sensitivity := 0.2
+@export var rotation_speed := 10.0
+
+@export_category("Move Feeling")
+@export var move_startup_time := 0.6
+@export var move_stop_time := 0.3
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_anchor = %CameraAnchor
-
 @onready var model: ChrModel = $Model
 @onready var anim_tree :AnimationTree= model.anim_tree
 
 var camera_input: Vector2
+var move_input: Vector2
 
 var camera_pivot: Node3D
 var camera: Camera3D
@@ -30,20 +35,19 @@ func _unhandled_input(event):
 
 func _process(_delta):
 	camera_input = Vector2.ZERO
+	move_input = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	if camera_pivot != null:
-		var mouse_sens := 100.0
-		camera_pivot.global_rotate(Vector3.UP, camera_input.x * mouse_sens * delta)
-		camera_pivot.global_rotate(camera_pivot.basis.x, camera_input.y * mouse_sens * delta)
+		camera_pivot.global_rotate(Vector3.UP, camera_input.x * delta)
+		camera_pivot.global_rotate(camera_pivot.basis.x, camera_input.y * delta)
 		var euler := camera_pivot.global_basis.get_euler(EULER_ORDER_YXZ)
-		euler.x = clamp(euler.x, deg_to_rad(-30.0), deg_to_rad(30.0))
+		euler.x = clamp(euler.x, deg_to_rad(-30.0), deg_to_rad(10.0))
 		camera_pivot.global_rotation = euler
 
-		var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 		var cam_forward = camera_pivot.global_transform.basis.z
 		cam_forward.y = 0
 		cam_forward = cam_forward.normalized()
@@ -51,20 +55,15 @@ func _physics_process(delta):
 		cam_right.y = 0
 		cam_right = cam_right.normalized()
 
-		var target_direction = (cam_right * input_dir.x + cam_forward * -input_dir.y).normalized()
-
-		if input_dir:
+		if move_input:
 			anim_tree.set("parameters/IdleWalk/blend_position", 1.0)
+			var target_direction = (cam_right * move_input.x + cam_forward * -move_input.y).normalized()
 			rotation.y = lerp_angle(rotation.y, atan2(target_direction.x, target_direction.z), rotation_speed * delta)
 		else:
 			anim_tree.set("parameters/IdleWalk/blend_position", 0.0)
 
 		var velocity_y := velocity.y
-		# if input_dir:
 		var root_motion_position :Vector3= model.get_root_motion_position()
 		velocity = root_motion_position / delta
-		# else:
-		# 	velocity = Vector3.ZERO
 		velocity.y = velocity_y
-		# print("velocity: %s" % [ velocity ])
 	move_and_slide()
